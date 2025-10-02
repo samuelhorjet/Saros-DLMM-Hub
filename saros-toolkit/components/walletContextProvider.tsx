0d3842eb11cc20853ffb1d0e04f921c7ad1671e2
samuelhorjet <samuelhorjet001@gmail.com>
2025-10-02 05:31:31 +0100

    Your message here

--- saros-toolkit/components/walletContextProvider.tsx
+++ saros-toolkit/components/walletContextProvider.tsx
@@ -6,8 +6,6 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 
-require('@solana/wallet-adapter-react-ui/styles.css');
-
 interface WalletContextProviderProps {
     children: ReactNode;
 }
